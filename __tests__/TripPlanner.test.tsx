17b79a2c1187d67b67ffaf261c52c695fbbddc52
dennistruong <truong.dennis00@gmail.com>
2023-07-24 02:40:19 -0700

    search tests pass with two way search

--- __tests__/TripPlanner.test.tsx
+++ __tests__/TripPlanner.test.tsx
@@ -30,7 +30,7 @@ const expectedLongPath = [
     stationHundredFourtyEighty,
   ],
 ];
-test("findViableTrips; return one path, deadend start", () => {
+test("findViableTrips; return one path, deadend start, one way", () => {
   expect(findViableTrips(largeGraph, stationFourSeven, 1)).toStrictEqual(
     expectedShortPath
   );
@@ -56,4 +56,38 @@ test("findViableTrips; return one path, deadend start", () => {
     expectedLongPath
   );
   expect(findViableTrips(largeGraph, stationFourSeven, 100)).toStrictEqual([]); // no viable path
+});
+
+// (4, 7) -3- (8, 7) -5- (42, 0) -68- (140,80)
+// const largeGraph = new Graph();
+// const stationFourSeven = newStation(4, 7);
+// const stationEightSeven = newStation(8, 7);
+// const stationFourtyTwoZero = newStation(42, 0);
+// const stationHundredFourtyEighty = newStation(140, 80);
+// largeGraph.addEdge(stationFourSeven, stationEightSeven, 3);
+// largeGraph.addEdge(stationEightSeven, stationFourtyTwoZero, 5);
+// largeGraph.addEdge(stationFourtyTwoZero, stationHundredFourtyEighty, 68);
+// // expected result
+// const expectedShortPath = [[stationFourSeven, stationEightSeven]];
+
+const expectedMiddleShortPath = [[stationEightSeven, stationFourtyTwoZero]];
+const expectedMiddleMedPath = [
+  [stationEightSeven, stationFourSeven],
+  [stationEightSeven, stationFourtyTwoZero],
+];
+const expectedMiddleLongPath = [
+  [stationEightSeven, stationFourtyTwoZero, stationHundredFourtyEighty],
+];
+
+test("findViableTrips; return two paths, middle start, two way", () => {
+  expect(findViableTrips(largeGraph, stationEightSeven, 2)).toStrictEqual(
+    expectedMiddleMedPath
+  );
+  expect(findViableTrips(largeGraph, stationEightSeven, 4)).toStrictEqual(
+    expectedMiddleShortPath
+  );
+  expect(findViableTrips(largeGraph, stationEightSeven, 6)).toStrictEqual(
+    expectedMiddleLongPath
+  );
+  expect(findViableTrips(largeGraph, stationEightSeven, 100)).toStrictEqual([]);
 });
