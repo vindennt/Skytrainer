6fda673d621bfc45257b6eec3d6d51ca0def4bb5
dennistruong <truong.dennis00@gmail.com>
2023-08-11 19:39:33 -0700

    removed redundant fragment system

--- app/screens/Team.tsx
+++ app/screens/Team.tsx
@@ -20,7 +20,7 @@ import { getStationName } from "../../utils/SkytrainData";
 import Popup from "../../components/Popup";
 import { Reward, Tier, gachaRoll } from "../../utils/GachaHandler";
 import {
-  fragmentLevelUp,
+  stationLevelUp,
   gachaPurchase,
   // giveFragment,
   unlockStation,
@@ -28,7 +28,7 @@ import {
 import moment from "moment";
 import { Character } from "../../components/TripMenu";
 import GridSelector from "../../components/GridSelector";
-import { LEVELUP_FRAGMENT_COST } from "../../utils/UnlockHandler";
+import { LEVELUP_COSTS } from "../../utils/UnlockHandler";
 
 const Team = () => {
   const [user, setUser] = useState<User | null>(null);
@@ -49,8 +49,8 @@ const Team = () => {
 
   // var unlockedCharList: Character[] = [];
   const [character, setCharacter] = useState<string>("001");
-  const [levelDisplay, setLevelDisplay] = useState<string>("0");
-  const [fragmentDisplay, setFragmentDisplay] = useState<number>(0);
+  const [levelDisplay, setLevelDisplay] = useState<number>(0);
+  // const [fragmentDisplay, setFragmentDisplay] = useState<number>(0);
   const [unlockedDateDisplay, setUnlockedDateDisplay] = useState<string>("");
 
   const setDisplayInfo = async (id: string) => {
@@ -60,9 +60,15 @@ const Team = () => {
     const docRef = doc(FIRESTORE_DB, `users/${uid}/characters/${id}`);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-      setLevelDisplay(docSnap.data().level);
-      setFragmentDisplay(docSnap.data().fragments);
-      if (docSnap.data().fragments >= LEVELUP_FRAGMENT_COST) {
+      await setLevelDisplay(docSnap.data().level);
+      // await setFragmentDisplay(docSnap.data().fragments);
+      console.log(
+        "Team: chekcing if can upgrade: $" +
+          money +
+          " VS cost: " +
+          LEVELUP_COSTS[levelDisplay - 1]
+      );
+      if (money >= LEVELUP_COSTS[levelDisplay - 1]) {
         setCanUpgrade(true);
       } else {
         setCanUpgrade(false);
@@ -122,9 +128,11 @@ const Team = () => {
           } as Character); // necessary line to pass typecheck
         });
         setUnlockedCharList(fetchedChars); // set displayed list to fetched array
-        setDisplayInfo(character);
-        if (isFocused && unlockedCharList.length > 0) {
+        if (unlockedCharList.length > 0) {
           setCharacter(unlockedCharList[0].id);
+          setDisplayInfo(character);
+        } else {
+          console.log("Team: emoty Unlocked char list ");
         }
 
         // console.log("GETTING TEAM DISPLAY");
@@ -170,6 +178,7 @@ const Team = () => {
       setMoney(userData?.money);
       // console.log("Pity: " + pity);
     });
+
     return () => unsub();
   }, [auth, displayName, uid]);
 
@@ -182,7 +191,7 @@ const Team = () => {
     if (docSnap.exists()) {
       const currentFragments: number = docSnap.data().fragments;
       console.log("Attempting level up for " + character);
-      await fragmentLevelUp(character, currentFragments, uid);
+      await stationLevelUp(character, money, levelDisplay, uid);
       setDisplayInfo(character);
       setCanUpgrade(true);
       setLoadingData(false);
@@ -232,12 +241,12 @@ const Team = () => {
         </View>
         <View style={styles.infoContainer}>
           <Text style={styles.text}>Level: {levelDisplay}</Text>
-          <Text style={styles.text}>Fragments: {fragmentDisplay}</Text>
+          {/* <Text style={styles.text}>Fragments: {fragmentDisplay}</Text> */}
         </View>
         <View style={styles.levelContainer}>
           {/* <IconButton icon="trash-can-outline" size={20} iconColor="red" /> */}
           <PaperButton
-            icon="puzzle"
+            icon="cash-multiple"
             disabled={!canUpgrade}
             loading={loadingData}
             style={styles.button}
@@ -247,7 +256,7 @@ const Team = () => {
             onPressOut={levelUp}
           >
             <Text style={styles.text}>
-              x{LEVELUP_FRAGMENT_COST} {"  "} Level Up
+              {LEVELUP_COSTS[levelDisplay - 1]} {"  "} Level Up
             </Text>
           </PaperButton>
         </View>
