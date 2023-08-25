461e408342bd5732d418b96c2d85c063d8e4e7ff
dennistruong <truong.dennis00@gmail.com>
2023-08-25 02:25:00 -0700

    added test button to confirm UI updates with state

--- src/screens/Shop.tsx
+++ src/screens/Shop.tsx
@@ -2,8 +2,15 @@ import * as React from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector, useDispatch } from "react-redux";
+import { UserState, setBalance } from "@src/features/user/userSlice";
 
 const Shop = () => {
+  const dispatch = useDispatch<any>();
+  const balance = useSelector(
+    (state: { user: UserState }) => state.user.balance
+  );
+
   const navigation = useNavigation();
 
   const goToGacha = () => {
@@ -14,6 +21,14 @@ const Shop = () => {
     <View style={styles.container}>
       <Text>Shop</Text>
       <Button onPress={goToGacha}>Gacha</Button>
+      <Button
+        onPress={() => {
+          console.log(balance);
+          dispatch(setBalance(balance + 100));
+        }}
+      >
+        Gain 100 coins
+      </Button>
     </View>
   );
 };
