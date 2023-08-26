92ab6d0b08da8246fa0f76681e9ee128946c2c75
dennistruong <truong.dennis00@gmail.com>
2023-08-26 01:23:45 -0700

    added state control for skytrain trip's state

---
+++ src/features/skytrainTrip/skytrainTripSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Graph, Station } from "@features/skytrainTrip/Graph";
+import { Reward } from "@features/reward/TripRewardHandler";
+import { buildGraph } from "./SkytrainData";
+
+// skytrainGraph: Graph of the skytrain stations
+// DEPRECATED: selectedStartStation: starting station for a trip NOTE: for now, try and use the stations selector to avoid redundancy. Stations UI should auto-scroll to the selected station
+// viableTrip: the randomly selected viable trip from among all the trips return from TripFinder
+// tripRewards: All rewards from the trip
+export interface SkytrainTripState {
+  skytrainGraph: Graph;
+  viableTrips: Station[][];
+  chosenViableTrip: Station[];
+  tripRewards: Reward[];
+  //   selectedStartStation: Station | null;
+}
+
+const initialState: SkytrainTripState = {
+  skytrainGraph: buildGraph(1),
+  viableTrips: [],
+  chosenViableTrip: [],
+  tripRewards: [],
+  //   selectedStartStation: null,
+};
+
+const skytrainTripSlice = createSlice({
+  name: "skytrainTrip",
+  initialState,
+  reducers: {
+    setViableTrips: (state, action) => {
+      state.viableTrips = action.payload;
+    },
+    setChosenViableTrip: (state, action) => {
+      state.chosenViableTrip = action.payload;
+    },
+    setTripRewards: (state, action) => {
+      state.tripRewards = action.payload;
+    },
+  },
+});
+
+export const { setViableTrips, setChosenViableTrip, setTripRewards } =
+  skytrainTripSlice.actions;
+export default skytrainTripSlice.reducer;
--- src/features/store.ts
+++ src/features/store.ts
@@ -1,10 +1,11 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 import authReducer from "@features/auth/authSlice";
 import userReducer from "@features/user/userSlice";
 import stationsReducer from "@features/stations/stationsSlice";
+import skytrainTripReducer from "@features/skytrainTrip/skytrainTripSlice";
 
 const persistConfig = {
   key: "root",
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
   stations: stationsReducer,
+  skytrainTrip: skytrainTripReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
