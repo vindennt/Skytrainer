37248671d3f21ed4a7e302ec0585445af399f693
dennistruong <truong.dennis00@gmail.com>
2023-08-29 19:07:45 -0700

    updated slider to fit ios style

--- src/components/TimeSlider.tsx
+++ src/components/TimeSlider.tsx
@@ -3,12 +3,8 @@ import { UserState, setSlider } from "@src/features/user/userSlice";
 import { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
+import Icon from "react-native-vector-icons/Ionicons";
 
-// interface SliderProps {
-//   onValueChange: (value: number) => void;
-// }
-
-// export const TimeSlider: React.FC<SliderProps> = ({ onValueChange }) => {
 export const TimeSlider: React.FC = () => {
   const dispatch = useDispatch();
   const sliderValue = useSelector(
@@ -32,31 +28,53 @@ export const TimeSlider: React.FC = () => {
   };
 
   return (
-    <Slider
-      style={styles.slider}
-      minimumValue={slider.min}
-      maximumValue={slider.max}
-      lowerLimit={slider.min}
-      upperLimit={slider.max}
-      value={sliderInitial} // initial value
-      step={slider.visualStep}
-      minimumTrackTintColor="whitesmoke"
-      maximumTrackTintColor="gray"
-      tapToSeek={true}
-      thumbTintColor="whitesmoke"
-      onValueChange={(value) => {
-        getNewValue(value);
-      }}
-      // onSlidingStart={() => setSliding("Sliding")}
-      // onSlidingComplete={() => setSliding("Inactive")}
-    />
+    <View style={styles.sliderBackdrop}>
+      <Slider
+        style={styles.slider}
+        minimumValue={slider.min}
+        maximumValue={slider.max}
+        lowerLimit={slider.min}
+        upperLimit={slider.max}
+        value={sliderInitial} // initial value
+        step={slider.visualStep}
+        minimumTrackTintColor="whitesmoke"
+        maximumTrackTintColor="darkgray"
+        tapToSeek={true}
+        thumbTintColor="white"
+        onValueChange={(value) => {
+          getNewValue(value);
+        }}
+        // onSlidingStart={() => setSliding("Sliding")}
+        // onSlidingComplete={() => setSliding("Inactive")}
+      />
+      <View style={styles.icon}>
+        <Icon name="timer-outline" size={18} color="gray" style={styles.icon} />
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   slider: {
-    width: "90%",
+    // width: "90%",
     height: 40,
     paddingHorizontal: 10,
+    // backgroundColor: "whitesmoke",
+    // borderRadius: 20,
+    bottom: 5,
+    // zIndex: 1,
+  },
+  sliderBackdrop: {
+    height: 29,
+    // paddingHorizontal: 10,
+    backgroundColor: "whitesmoke",
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+  icon: {
+    padding: 4,
+    marginLeft: 0.5,
+    position: "absolute",
   },
 });
