86de3dcb0a91a30bb0c63427ca40b58eb56c2695
dennistruong <truong.dennis00@gmail.com>
2023-08-30 00:44:58 -0700

    finalized renaming of slice that caused all the problems

--- src/components/TripBox.tsx
+++ src/components/TripBox.tsx
@@ -20,7 +20,7 @@ export const TripBox: React.FC = () => {
     (state: { stations: StationsState }) => state.stations.selectedStation
   );
   const skytrainGraph = useSelector(
-    (state: { skytrainTrip: SkytrainState }) => state.skytrainTrip.skytrainGraph
+    (state: { skytrain: SkytrainState }) => state.skytrain.skytrainGraph
   );
 
   const handleTripStart = () => {
